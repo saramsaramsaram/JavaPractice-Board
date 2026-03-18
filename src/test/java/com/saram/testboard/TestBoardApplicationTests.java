cc5ecce2cc067a63a6bd02fdfc4d3f15043c8ae1
saramsaramsaram <leezuhwan000@dgsw.hs.kr>
2026-03-18 20:13:22 +0900

    first commit

---
+++ src/test/java/com/saram/testboard/TestBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saram.testboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
